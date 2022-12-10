d7432835185aeb3a158a1a67bed632e846010d12
Debanjana <debanjana.9806@gmail.com>
2022-12-09 19:14:19 -0800

    submit

--- solution.js
+++ solution.js
@@ -2,5 +2,13 @@
 
 function palindrome(str){
 
-}
-module.exports = palindrome
+    str = str.toLowerCase().replace(/(\W)|(\_)/g,"").split("");
+    for (var i = 0; i < str.length; i++) {
+        if (str[i] !== str[str.length - 1 -i]) {
+            return false;
+        }
+    }
+    return true;
+}   
+  
+   module.exports = palindrome
